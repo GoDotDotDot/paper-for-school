f6f4381bb202cd87a113f409110d689db696757c
KUI CHU <chukui0202@gmail.com>
2017-11-27 15:38:49 +0800

    Update db.config.js

--- code/__server__/db.config.js
+++ code/__server__/db.config.js
@@ -3,7 +3,7 @@ const DB_CONNET =
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
-    password: 'cuit',
+    password: 'your_password',
     database: 'school'
   }
 module.exports = DB_CONNET
